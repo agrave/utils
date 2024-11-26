a529b497631ff07c780da26bf9c721656e020f83
Artem Mohylnykov <agrave18@gmail.com>
2024-11-26 07:14:39 +0200

    initial commit

---
+++ powershell/subfolder-size.ps1
@@ -0,0 +1,19 @@
+﻿#Get-ChildItem $directory | select -Expand Name | $PSScriptRoot
+
+# Get the current directory
+$currentFolder = Get-Location
+
+# Get all subfolders in the current directory
+$subfolders = Get-ChildItem -Directory -Path $currentFolder
+
+# Iterate through each subfolder and calculate its size
+$results = foreach ($subfolder in $subfolders) {
+    $size = (Get-ChildItem -Path $subfolder.FullName -Recurse -File -ErrorAction SilentlyContinue | Measure-Object -Property Length -Sum).Sum
+    [PSCustomObject]@{
+        Subfolder = $subfolder.Name
+        SizeGB    = [math]::Round($size / 1GB, 2)
+    }
+}
+
+# Display the results
+$results | Sort-Object SizeGB -Descending | Format-Table -AutoSize
